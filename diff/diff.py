cefd4320b74dc6f4406336ddb1ed0a57c9275379
Daniel Mathews <danielbmathews@gmail.com>
2016-02-11 07:28:51 -0800

    Generalize to arbitrary lists, keep order of diffs

--- diff/diff.py
+++ diff/diff.py
@@ -18,34 +18,8 @@ class TestResults(object):
     def display_results(self):
         pass
 
-def compare_column_names(df1, df2):
-    results = dict()
-    df1_cols = df1.columns.tolist()
-    df2_cols = df2.columns.tolist()
-    # Are the columns identical
-    if df1_cols == df2_cols:
-        results['column names'] = 'same, including order'
-    elif set(df1_cols) == set(df2_cols):
-        results['column names'] = 'same, but different order'
-    else:
-        results['column names'] = 'different'
 
-    # Does df2 have additional columns?
-    df2_added_columns = list(set(df2_cols) - set(df1_cols))
-    results['added column(s)'] = df2_added_columns
-
-    # Does df2 have missing columns?
-    df2_missing_columns = list(set(df1_cols) - set(df2_cols))
-    results['missing column(s)'] = df2_missing_columns
-    return results
-
-# TODO(daniel) General compare_* function. Comparing column names or rows or
-# anything is fundamentally the same operation. Comparing column names is a
-# standard call. Comparing dataframes rows take two iterations i.e. one to
-# find which rows are different, second to identify individual values that
-# are different.
-
-def compare_lists(A, B):
+def diff_lists(A, B):
     results = dict()
     # Check for equality, with order & ignoring order
     if A == B:
@@ -54,31 +28,14 @@ def compare_lists(A, B):
         results['equality check without order'] = True
     else:
         results['elements'] = 'Different'
-    # check for added elements
-    added = list(set(A) - set(B))
-    if added:
-        results['added elements'] = added
-    # check for lost elements
-    lost = list(set(B) - set(A))
-    if lost:
-        results['lost elements'] = lost
+    # Check for added and lost elements
+    results['added elements'] = [b for b in B if b not in A]
+    results['lost elements'] =  [a for a in A if a not in B]
+
     return results
 
-def diff_lists(list_1, list_2):
-    results = dict()
-    # Check for equality, with order & ignoring order
-    if list_1 == list_2:
-        results['equality check with order'] = True
-    elif set(list_1) == set(list_2):
-        results['equality check without order'] = True
-    else:
-        results['elements'] = 'Different'
-    # check for added elements
-    added = list(set(list_1) - set(list_2))
-    if added:
-        results['added elements'] = added
-    # check for lost elements
-    lost = list(set(list_2) - set(list_1))
-    if lost:
-        results['lost elements'] = lost
-    return results
+# TODO(daniel) General compare_* function. Comparing column names or rows or
+# anything is fundamentally the same operation. Comparing column names is a
+# standard call. Comparing dataframes rows take two iterations i.e. one to
+# find which rows are different, second to identify individual values that
+# are different.
--- diff/test_diff.py
+++ diff/test_diff.py
@@ -1,31 +1,12 @@
-import pandas as pd
-from diff import compare_column_names, diff_lists
+from diff import diff_lists
 
-
-def test_compare_column_names():
-    df1 = pd.DataFrame({'a':[1, 2, 3], 'b':[2, 2, 2]})
-    df2 = pd.DataFrame({'a':[1, 2, 3], 'b':[2, 2, 2]})
-    df3 = pd.DataFrame({'a':[1, 2, 3], 'c':[2, 2, 2]})
-
-    # comapare df1 and df2 columsn - which are identical
-    assert compare_column_names(df1, df2)['added column(s)'] == []
-    assert compare_column_names(df2, df1)['added column(s)'] == []
-    assert compare_column_names(df2, df1)['missing column(s)'] == []
-
-    # compare df1 and df3 columsn - which are different
-    assert compare_column_names(df1, df3)['added column(s)'] == ['c']
-    assert compare_column_names(df1, df3)['missing column(s)'] == ['b']
+list_1 = [1, 2, 3]
+list_2 = [2, 1, 4]
 
 def test_diff_lists():
-    df1 = pd.DataFrame({'a':[1, 2, 3], 'b':[2, 2, 2]})
-    df2 = pd.DataFrame({'a':[1, 2, 3], 'b':[2, 2, 2]})
-    df3 = pd.DataFrame({'a':[1, 2, 3], 'c':[2, 2, 2]})
-
-    # comapare df1 and df2 columsn - which are identical
-    assert diff_lists(df1.columns, df2.columns)['added column(s)'] == []
-    assert diff_lists(df2.columns, df1.columns)['added column(s)'] == []
-    assert diff_lists(df2.columns, df1.columns)['missing column(s)'] == []
+    assert diff_lists(list_1, list_1)['added elements'] == []
+    assert diff_lists(list_1, list_1)['lost elements'] == []
 
     # compare df1 and df3 columsn - which are different
-    assert diff_lists(df1.columns, df3.columns)['added column(s)'] == ['c']
-    assert diff_lists(df1.columns, df3.columns)['missing column(s)'] == ['b']
+    assert diff_lists(list_1, list_2)['added elements'] == [4]
+    assert diff_lists(list_1, list_2)['lost elements'] == [3]
